4da5566fbb52e808fb4c6c5707d927dc6e9fd410
rustico <nmbases@gmail.com>
2015-01-12 01:10:11 -0300

    Migration // Django Rest Framework 3.0 // Contact

--- web-api/contact/serializers.py
+++ web-api/contact/serializers.py
@@ -5,3 +5,10 @@ class ContactSerializer(serializers.ModelSerializer):
     class Meta:
         model = Contact
         fields = ('subject', 'text')
+
+    def create(self, validated_data):
+        contact = Contact(**validated_data)
+        contact.player = self.context['request'].user
+        contact.save()
+
+        return contact
